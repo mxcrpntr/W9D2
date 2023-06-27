3c31c5fc69a5597bce1272e5305d730af7b0d39d
mxcrpntr <carpenter.max@gmail.com>
2023-06-27 14:17:24 -0400

    messing with ttt-view

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,8 +1,18 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
   }
   
   setupBoard() {
+    const ul =  document.createElement("ul");
+    for (let i = 0; i < 9; i++) {
+      let li =  document.createElement("li");
+      li.setAttribute("data-row", Math.floor(i / 3) + 1);
+      li.setAttribute("data-col", (i % 3) + 1 );
+      ul.appendChild(li);
+    }
+    this.el.appendChild(ul);
   }
   
   handleClick(e) {
